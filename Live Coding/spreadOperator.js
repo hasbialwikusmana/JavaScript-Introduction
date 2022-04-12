81186e30df53e5055ef4c22a20d7baa47d6bcdff
Hasbi Alwi Kusmana <hasbialwik@gmail.com>
2022-04-12 20:28:47 +0700

    Create spreadOperator.js

---
+++ Live Coding/spreadOperator.js
@@ -0,0 +1,12 @@
+let myHobby = ['swiming', 'football', 'coding'];
+
+// memasukan array ke dalam array lain
+// const myHobby2 = ['nyanyi','menari',...myHobby];
+
+// menggabungkan 2 array
+// const resultHobby = [...myHobby,...myHobby2];
+
+// copy
+const resultMyHobby = [...myHobby];
+resultMyHobby[1] = 'desain';
+console.log(resultMyHobby);
