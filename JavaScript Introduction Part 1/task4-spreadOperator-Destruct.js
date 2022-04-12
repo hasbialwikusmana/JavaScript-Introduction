73daceb7a3e7c72c38c686a545e6fccb13523ac6
Hasbi Alwi Kusmana <hasbialwik@gmail.com>
2022-04-12 20:29:06 +0700

    Create task4-spreadOperator-Destruct.js

---
+++ JavaScript Introduction Part 1/task4-spreadOperator-Destruct.js
@@ -0,0 +1,28 @@
+let data = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: 
+    {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    }
+
+    
+//  a. Ubahlah data  tersebut menggunakan spread operator menjadi: 
+    // name: nama anda
+    // email: email anda
+    // hobby: hobi anda
+
+    const resultData = {...data, name: "Hasbi Alwi Kusmana", email: "hasbialwi70@gmail.com", myHobby: ['coding', 'reading', 'listening to music']};
+    // console.log(resultData);
+
+//  b. Ambilah data “street dan city” tersebut menggunakan destructuring
+    const {street, city} = data.address;
+    console.log(street, city);
