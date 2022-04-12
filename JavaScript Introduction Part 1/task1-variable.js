1b5c21746787c182cbbdce1e6a3f3b26313731eb
Hasbi Alwi Kusmana <hasbialwik@gmail.com>
2022-04-12 20:28:56 +0700

    Create task1-variable.js

---
+++ JavaScript Introduction Part 1/task1-variable.js
@@ -0,0 +1,37 @@
+const biodata = {
+	name: "Hasbi Alwi Kusmana",
+	age: 23,
+    hobbies: ['coding', 'reading', 'listening to music'],
+    isMarried: false,
+    schoolList: [
+        {
+        name: 'SD Negeri 1 Sukasenang',
+        yearIn: 2006,
+        yearOut: 2012,
+        major: null
+    }, {
+        name: 'Mts N 1 Sindangkasih',
+        yearIn: 2012,
+        yearOut: 2015,
+        major: null
+    }, {
+        name: 'SMK Negeri 2 Ciamis',
+        yearIn: 2015,
+        yearOut: 2018,
+        major: 'Teknik Otomotif'
+    }],
+    skills: [
+        {
+        skillName: 'HTML',
+        level: 'beginner'
+    }, {
+        skillName: 'CSS',
+        level: 'beginner'
+    }, {
+        skillName: 'Javascript',
+        level: 'beginner'
+    }],
+    interest_in_coding: true
+}
+
+console.log(biodata);
