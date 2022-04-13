dcd04171d171d546aea4aaf1d571c8e63334664c
Hasbi Alwi Kusmana <hasbialwik@gmail.com>
2022-04-13 10:10:43 +0700

    Update spreadOperator.js

--- Live Coding/spreadOperator.js
+++ Live Coding/spreadOperator.js
@@ -10,3 +10,26 @@ let myHobby = ['swiming', 'football', 'coding'];
 const resultMyHobby = [...myHobby];
 resultMyHobby[1] = 'desain';
 console.log(resultMyHobby);
+
+// menggabungkan data menggunakan spread operator
+const myName = {
+    name : 'fahmi',
+    age : 20,
+    email:"fahmi@gmail.com"
+}
+
+const friendName = {
+    name : 'zarif',
+    age : 20,
+    address : 'jakarta'
+}
+
+// const newName = {
+//     ...myName,
+//     ...friendName
+// }
+
+const newName = {
+    ...myName,
+    age:25,
+}
