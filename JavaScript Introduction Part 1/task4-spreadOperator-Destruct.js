263b0ae79c6afffbf5ac020055304ed53d91b94d
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-14 09:46:06 +0700

    Update task4-spreadOperator-Destruct.js

--- JavaScript Introduction Part 1/task4-spreadOperator-Destruct.js
+++ JavaScript Introduction Part 1/task4-spreadOperator-Destruct.js
@@ -3,12 +3,11 @@ let data = {
     name: "Leanne Graham",
     username: "Bret",
     email: "Sincere@april.biz",
-    address: 
-    {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
+    address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
     },
     phone: "1-770-736-8031 x56442",
     website: "hildegard.org",
@@ -20,9 +19,9 @@ let data = {
     // email: email anda
     // hobby: hobi anda
 
-    const resultData = {...data, name: "Hasbi Alwi Kusmana", email: "hasbialwi70@gmail.com", myHobby: ['coding', 'Playing Games', 'music']};
-    // console.log(resultData);
+    const resultData = {...data, name: "Hasbi Alwi Kusmana", email: "hasbialwi70@gmail.com", hobby: ['coding', 'Playing Games', 'music']};
+    console.log(resultData);
 
 //  b. Ambilah data “street dan city” tersebut menggunakan destructuring
     const {street, city} = data.address;
-    console.log(street, city);
+    // console.log(street, city);
