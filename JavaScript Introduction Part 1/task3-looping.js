0e7e5d30e3984bf3f67b5ad962053c5aad6a79a4
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-14 09:46:02 +0700

    Update task3-looping.js

--- JavaScript Introduction Part 1/task3-looping.js
+++ JavaScript Introduction Part 1/task3-looping.js
@@ -1,14 +1,13 @@
-const printSegitiga = (num) => {
-    if (typeof num != "number") {
-        console.log("Data harus number");
-    } else {
-        for (let i = num; i >= 1; i--) {
+let printSegitiga = 6;
+if(typeof printSegitiga != "number"){
+    console.log("Data harus number");
+}
+else{
+    for(let i = printSegitiga; i >= 1; i--){
         let result = "";
-        for (let j = 1; j <= i; j++) {
+        for(let j = 1; j <= i; j++){
             result += " " + j;
         }
         console.log(result);
-        }
     }
-    }
-printSegitiga(6);
+}
