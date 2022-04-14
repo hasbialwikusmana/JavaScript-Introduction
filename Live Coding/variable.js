557e9507e71e9bc3b7bf0854c9f31d22d87ddb9b
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-14 15:25:39 +0700

    Update

--- Live Coding/variable.js
+++
@@ -1,5 +0,0 @@
-var firstName = "Hasbi";
-let email = "hasbialwi70@gmail.com";
-const age = 23;
-
-console.log(firstName);
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 JavaScript Introduction Lesson Part 1-2-3
 
 ## JavaScript Part 1
+
+## JavaScript Part 2
