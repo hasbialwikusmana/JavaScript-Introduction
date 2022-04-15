f9c474dacf805930f9a8fca3579cff11234c5efd
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-15 16:14:08 +0700

    Create task1-buildInFunction.js

---
+++ JavaScript Introduction Part 2/task1-buildInFunction.js
@@ -0,0 +1,80 @@
+// Build in Function
+// 1. filter
+    // berfungsi untuk mengambil data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.filter((value) => value > 5);
+    // console.log(result);
+    // output: [6, 7, 8, 9, 10]
+
+// 2. reduce
+    // berfungsi untuk menjumlahkan data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.reduce((number, value) => number + value);
+    // console.log(result);
+    // output: 55
+
+// 3. sort
+    // berfungsi untuk mengurutkan data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [2, 1, 5, 3, 4, 7, 8, 6, 10, 9];
+    // const result = data.sort((a, b) => a - b);
+    // console.log(result);
+    // output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// 4. slice
+    // berfungsi untuk mengambil data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.slice(0, 5);
+    // console.log(result);
+    // output: [1, 2, 3, 4, 5]
+
+// 5. indexOf
+    // berfungsi untuk mencari data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.indexOf(5);
+    // console.log(result);
+    // output: 4
+
+// 6. some
+    // berfungsi untuk mengecek apakah ada data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.some((value) => value > 5);
+    // console.log(result);
+    // output: true
+
+// 7. find
+    // berfungsi untuk mencari data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.find((value) => value > 5);
+    // console.log(result);
+    // output: 6
+
+// 8. includes
+    // berfungsi untuk mengecek apakah ada data yang sesuai dengan kondisi yang ditentukan
+    // contoh:
+    // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const result = data.includes(5);
+    // console.log(result);
+    // output: true
+
+// 9. toLowerCase
+    // berfungsi untuk mengubah data menjadi huruf kecil
+    // contoh:
+    // const data = "Hello World";
+    // const result = data.toLowerCase();
+    // console.log(result);
+    // output: "hello world"
+
+// 10. concat
+    // berfungsi untuk menggabungkan data
+    // contoh:
+    // const data = ["Hello", "World"];
+    // const result = data.concat("!");
+    // console.log(result);
+    // output: ["Hello", "World", "!"]
