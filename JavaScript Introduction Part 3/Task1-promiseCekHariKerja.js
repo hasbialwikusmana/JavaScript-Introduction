3cd140820180ae964fae2aaa4175fd8cf8059557
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-20 19:17:34 +0700

    Update Task1-promiseCekHariKerja.js

--- JavaScript Introduction Part 3/Task1-promiseCekHariKerja.js
+++ JavaScript Introduction Part 3/Task1-promiseCekHariKerja.js
@@ -17,26 +17,26 @@ const cekHariKerja = (day) => {
 //then berfungsi untuk handler ketika cek bernilai true
 //catch berfungsi untuk handler ketika cek bernilai false
 
-// cekHariKerja("senin")
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//   });
+cekHariKerja("senin")
+  .then((result) => {
+    console.log(`Hari ${result} adalah hari kerja`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 // try memungkinkan untuk mengecek atau memeriksa block agar mengetahui error atau tidak
 // // catch adalah block yang menampilkan error jika pada try terjadi error
 // // sedangkan finally akan menjalankan saat true ataupun false
 
-const jadwalKerja = async () => {
-  try {
-    const result = await cekHariKerja("sabtu");
-    console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    console.log("Program selesai!");
-  }
-};
-jadwalKerja();
+// const jadwalKerja = async () => {
+//   try {
+//     const result = await cekHariKerja("senin");
+//     console.log(`Hari ${result} adalah hari kerja`);
+//   } catch (error) {
+//     console.log(error.message);
+//   } finally {
+//     console.log("Program selesai!");
+//   }
+// };
+// jadwalKerja();
