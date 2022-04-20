10eb767231b0a7e346684f4dd077ffa3670d7073
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-20 19:33:06 +0700

    Update Task3-promiseBebas.js

--- JavaScript Introduction Part 3/Task3-promiseBebas.js
+++ JavaScript Introduction Part 3/Task3-promiseBebas.js
@@ -1,22 +1,22 @@
-// 1.Cek name
-const checkName = (name) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (name === "Hasbi") {
-        resolve(name);
-      } else {
-        reject(new Error("It's not my name"));
-      }
-    }, 3000);
-  });
-};
-checkName("Hasbi")
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+// 1.Cek nama
+// const checkName = (name) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (name === "Hasbi") {
+//         resolve(name);
+//       } else {
+//         reject(new Error("It's not my name"));
+//       }
+//     }, 3000);
+//   });
+// };
+// checkName("Hasbi")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
 
 // 2.Penjumlahan nilai awal dan nilai akhir
 // const getNilai = (nilaiAwal, nilaiAkhir, callback) => {
